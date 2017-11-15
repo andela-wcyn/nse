9760638f26cbe960eba710169772a2b3d7a6c8a4
andela-wcyn <cynthia.abura@andela.com>
2017-11-15 09:14:17 +0300

    [Feature] Add clock and scraper runner. Fix import error

--- nse/app/clock.py
+++ nse/app/clock.py
@@ -4,8 +4,8 @@ import sys
 from apscheduler.schedulers.blocking import BlockingScheduler
 from rq import Queue
 
-from nse.run import crawl_nse_site
-from worker import conn
+from nse.app.worker import conn
+from nse.app.run import crawl_nse_site
 
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 scheduler = BlockingScheduler()
--- nse/app/worker.py
+++ nse/app/worker.py
@@ -10,6 +10,9 @@ redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
 conn = redis.from_url(redis_url)
 
 if __name__ == '__main__':
- with Connection(conn):
- worker = Worker(map(Queue, listen))
- worker.work()
+    with Connection(conn):
+        worker = Worker(map(Queue, listen))
+        worker.work()
+
+# Very helpful resource:
+# https://bigishdata.com/2016/12/15/running-python-background-jobs-with-heroku/
