f7c31f17ab0e5b26e12eb0ae271fd0967fbbabe7
Cynthia Abura <wcyn@C4A.local>
2017-11-21 00:30:27 +0300

    [Chore] Change Heroku dyno type to worker

--- nse/app/clock.py
+++ nse/app/clock.py
@@ -5,7 +5,7 @@ import schedule
 import time
 
 # Adjusting to UTC time on Heroku Server (3 hours behind)
-schedule.every().day.at("21:25").do(crawl_nse_site)  # Every midday
+schedule.every().day.at("21:35").do(crawl_nse_site)  # Every midday
 print "Starting clock..."
 while True:
     schedule.run_pending()
