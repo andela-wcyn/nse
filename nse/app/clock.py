40f3f575a523fb7650d15d4c50293f75878312b0
Cynthia Abura <wcyn@C4A.local>
2017-11-21 00:24:27 +0300

    [Chore] Add print statement to show clock starting

--- nse/app/clock.py
+++ nse/app/clock.py
@@ -6,7 +6,7 @@ import time
 
 # Adjusting to UTC time on Heroku Server (3 hours behind)
 schedule.every().day.at("21:25").do(crawl_nse_site)  # Every midday
-
+print "Starting clock..."
 while True:
     schedule.run_pending()
     time.sleep(60)  # Wait one minute
