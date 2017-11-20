4fe983aff2f54d3a6e71d8ee4bbc45080ae58ad4
Cynthia Abura <wcyn@C4A.local>
2017-11-21 00:19:52 +0300

    [Chore] Change time to 9:25pm to test on Heroku

--- nse/app/clock.py
+++ nse/app/clock.py
@@ -5,7 +5,7 @@ import schedule
 import time
 
 # Adjusting to UTC time on Heroku Server (3 hours behind)
-schedule.every().day.at("05:15").do(crawl_nse_site)  # Every midday
+schedule.every().day.at("21:25").do(crawl_nse_site)  # Every midday
 
 while True:
     schedule.run_pending()
