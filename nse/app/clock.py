a744439bf9f78187d728c5a130d40d88ac5ec3fb
Cynthia Abura <wcyn@C4A.local>
2017-11-21 15:49:38 +0300

    [Chore] Add output to monitor clock

--- nse/app/clock.py
+++ nse/app/clock.py
@@ -7,6 +7,9 @@ import time
 # Adjusting to UTC time on Heroku Server (3 hours behind)
 schedule.every().day.at("21:35").do(crawl_nse_site)  # Every midday
 print "Starting clock..."
+count = 0
 while True:
+    print "Minute: ", count+1
     schedule.run_pending()
     time.sleep(60)  # Wait one minute
+    count+=1
